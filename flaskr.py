80e68a1fe158dcfbfed7a9d4b3aba287fd7f961b
Pete Marshall <petemarshall77@gmail.com>
2015-06-04 16:04:51 -0700

    Enable writing blog posts in Markdown

--- flaskr.py
+++ flaskr.py
@@ -6,6 +6,8 @@ from flask import Flask, request, session, g, redirect, url_for
 from flask import abort, render_template, flash
 from contextlib import closing
 import os
+import markdown
+from flask import Markup
 
 # Configuration
 DATABASE = 'data/flaskr.db'
@@ -66,7 +68,7 @@ def show_entries():
     """Show the blog entries on the home page"""
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    entries = [dict(title=row[0], text=Markup(markdown.markdown(row[1]))) for row in cur.fetchall()]
     return render_template('show_entries.html', entries = entries)
 
 
