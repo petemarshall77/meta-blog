3e5a12ce35c74943cf9cf91eb2a93ab2ddcd6528
Pete Marshall <petemarshall77@gmail.com>
2015-06-12 15:22:07 -0700

    Move OAuth keys to environment variables

--- flaskr.py
+++ flaskr.py
@@ -28,8 +28,8 @@ twitter = oauth.remote_app('twitter',
                            request_token_url = 'https://api.twitter.com/oauth/request_token',
                            access_token_url = 'https://api.twitter.com/oauth/access_token',
                            authorize_url = 'https://api.twitter.com/oauth/authorize',
-                           consumer_key = 'r73Aupw0YZxN0T7pimUS98yN4',
-                           consumer_secret = 'GFr2fpsK6MSf53odRutuD0k6eDwfWw0kyEppdfDRLcmjofxW9v')
+                           consumer_key = os.getenv('TWITTER_CONSUMER_KEY'),
+                           consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET'))
 
 # Create the application
 app = Flask(__name__)
